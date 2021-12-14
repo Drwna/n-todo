02f76068f1b76ad095c5c21b5146ae6d3e1cbbd7
draganet <3417237276@qq.com>
2021-12-15 00:35:24 +0800

    完成其余功能

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require("./db")
+const inquirer = require('inquirer');
 
 module.exports.create = async (title) => {
     // 读取之前的任务
@@ -44,7 +45,72 @@ module.exports.showAll = async () => {
     // 读取之前的任务
     const list = await db.read()
     // 打印之前的任务
-    list.forEach((task, index) => {
-        console.log(`${task.done ? '[⨉]' : '[_]'} ${index} - ${task.title}`)
-    })
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'index',
+            message: '请选择想要操作的任务',
+            choices: [{name: '退出', value: '-1'}, ...list.map((task, index) => {
+                return {name: `${task.done ? '[x]' : '[_]'} ${index} - ${task.title}`, value: index.toString()}
+            }), {name: '+ 添加任务', value: '-2'}]
+        })
+        .then((answer) => {
+            const index = parseInt(answer.index)
+            if (index >= 0) {
+                // 选中一个任务
+                inquirer.prompt({
+                    type: 'list',
+                    name: 'action',
+                    message: '请选择操作',
+                    choices: [
+                        {name: '退出', value: 'quit'},
+                        {name: '已完成', value: 'markAsDone'},
+                        {name: '未完成', value: 'markAsUndone'},
+                        {name: '修改标题', value: 'updateTitle'},
+                        {name: '删除', value: 'remove'}
+                    ]
+                }).then(answer2 => {
+                    switch (answer2.action) {
+                        case 'markAsDone':
+                            list[index].done = true
+                            db.write(list)
+                            break;
+                        case 'markAsUndone':
+                            list[index].done = false
+                            db.write(list)
+                            break;
+                        case  'updateTitle':
+                            inquirer.prompt({
+                                    type: 'input',
+                                    name: 'title',
+                                    message: "输入新的标题",
+                                    default: list[index].title
+                                },
+                            ).then((answer) => {
+                                list[index].title = answer.title
+                                db.write(list)
+                            })
+                            break;
+                        case 'remove':
+                            list.splice(index, 1)
+                            db.write(list)
+                            break;
+                    }
+                })
+            } else if (index === -2) {
+                // 创建任务
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'title',
+                    message: '请输入任务标题'
+                }).then(answer => {
+                    list.push({
+                        title: answer.title,
+                        done: false
+                    })
+                    db.write(list)
+                })
+            }
+            console.log(answer.index)
+        });
 }

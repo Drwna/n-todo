8778c085ad664eeb853570b9d707e51a95869b03
draganet <3417237276@qq.com>
2021-12-16 00:43:14 +0800

    jest 单元测试 写文件 write

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,10 +3,10 @@ const _fs = jest.requireActual('fs')
 
 Object.assign(fs, _fs)
 
-const mocks = {}
+const readMocks = {}
 
-fs.setMock= (path, error, data) => {
-    mocks[path] = [error, data]
+fs.setReadFileMock = (path, error, data) => {
+    readMocks[path] = [error, data]
 }
 
 // fs.readFile('xx',fn)
@@ -14,11 +14,25 @@ fs.readFile = (path, option, callback) => {
     if (option === undefined) {
         callback = option
     }
-    if (path in mocks) {
-        callback(...mocks[path])
-        // callback(mocks[path][0], mocks[path][1])
+    if (path in readMocks) {
+        callback(...readMocks[path])
+        // callback(readMocks[path][0], readMocks[path][1])
     } else {
         _fs.readFile(path, option, callback)
+    }
+}
+
+const writeMocks = {}
+
+fs.setWriteFileMock = (path, fn) => {
+    writeMocks[path] = fn
+}
+
+fs.writeFile = (path, data, option, callback) => {
+    if (path in writeMocks) {
+        writeMocks[path](path, data, option, callback)
+    } else {
+        _fs.writeFile(path, data, option, callback)
     }
 }
 
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,12 +1,22 @@
 const db = require('../db.js')
-const fs = require("fs")
+const fs = require('fs')
 jest.mock('fs')
 
 describe('db', () => {
     it('can read', async () => {
         const data = [{title: 'hi', done: true}]
-        fs.setMock('/xxx', null, JSON.stringify(data))
+        fs.setReadFileMock('/xxx', null, JSON.stringify(data))
         const list = await db.read('/xxx')
         expect(list).toStrictEqual(data)
+    })
+    it('can write', async () => {
+        let fakeFile
+        fs.setWriteFileMock('/yyy', (path, data, callback) => {
+            fakeFile = data
+            callback(null)
+        })
+        const list = [{title: 'i am iron man', done: true}, {title: 'i am spider-man', done: true}]
+        await db.write(list, '/yyy')
+        expect(fakeFile).toBe(JSON.stringify(list))
     })
 })
